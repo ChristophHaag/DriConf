a99a065a7021ed9a045ad0b4fcc09775ef3f02a6
felix <felix@b79d2f87-9f1e-0410-80e5-81984dd5ec66>
2003-07-16 15:34:19 +0000

    Imported version 0.0.1.

--- driconf/branches/felix/install.sh
+++ driconf/branches/felix/install.sh
@@ -9,13 +9,14 @@ SOURCES="dri driconf driconf_tb_icons"
 for x in $SOURCES; do
     echo -n "Byte compiling $x.py ... "
     rm -f $x.pyo $x.pyc
-    echo "import ${x}" | $PYTHON -O
+    echo "import ${x}" | $PYTHON
     echo "done."
 done
 
+mkdir -p $PREFIX/lib/$PYTHON/site-packages
 for x in $SOURCES; do
-    echo -n "Installing $x.pyo to $PREFIX/lib/$PYTHON ... "
-    install -m 644 -o root $x.pyo $PREFIX/lib/$PYTHON
+    echo -n "Installing $x.pyc to $PREFIX/lib/$PYTHON/site-packages ... "
+    install -m 644 -o root $x.pyc $PREFIX/lib/$PYTHON/site-packages
     echo "done."
 done
 echo -n "Installing driconf to $PREFIX/bin ... "
