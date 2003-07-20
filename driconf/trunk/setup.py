43c020229f1954ab3f478a73627e8cad16462d66
felix <felix@b79d2f87-9f1e-0410-80e5-81984dd5ec66>
2003-07-20 13:57:39 +0000

    Check for gtk and expat in setup.py.

--- driconf/trunk/setup.py
+++ driconf/trunk/setup.py
@@ -8,3 +8,18 @@ setup(name="driconf",
       url="http://fxk.de.vu/projects_cur_en",
       py_modules=["dri", "driconf", "driconf_xpm"],
       scripts=["driconf"])
+
+errors = 0
+try:
+    import gtk
+except ImportError:
+    print "Warning: importing gtk doesn't work."
+    errors = 1
+try:
+    import xml.parsers.expat
+except ImportError:
+    print "Warning: importing xml.parsers.expat doesn't work."
+    errors = 1
+
+if errors:
+    print "Warning: driconf will probably not work for the above reason(s)."
