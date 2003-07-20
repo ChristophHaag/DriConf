a394d5131669013ff109e3f0eb9b3c974b078821
felix <felix@b79d2f87-9f1e-0410-80e5-81984dd5ec66>
2003-07-20 13:17:17 +0000

    Replaced Makefile with setup.py/setup.cfg.

---
+++ driconf/trunk/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="driconf",
+      version="0.0.3",
+      description="DRI Configuration GUI",
+      author="Felix Kuehling",
+      author_email="fxkuehl@gmx.de",
+      url="http://fxk.de.vu/projects_cur_en",
+      py_modules=["dri", "driconf", "driconf_xpm"],
+      scripts=["driconf"])
