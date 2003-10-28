70c4e34057b5421023efeadb772ce50684046b08
felix <felix@b79d2f87-9f1e-0410-80e5-81984dd5ec66>
2003-10-28 14:55:18 +0000

    Final touches for release 0.1.0.

--- driconf/branches/gtk-1-2-branch/setup.py
+++ driconf/branches/gtk-1-2-branch/setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(name="driconf",
-      version="0.0.11",
-      description="DRI Configuration GUI",
+      version="0.1.0",
+      description="DRI Configuration GUI (gtk-1.2)",
       author="Felix Kuehling",
       author_email="fxkuehl@gmx.de",
-      url="http://fxk.de.vu/projects_cur_en",
+      url="http://dri.sourceforge.net/cgi-bin/moin.cgi/DriConf",
       py_modules=["dri", "driconf", "driconf_xpm"],
       scripts=["driconf"])
 
