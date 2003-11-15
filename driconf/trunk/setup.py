ab090c8465ac5755def2d83be396e9a806b50838
felix <felix@b79d2f87-9f1e-0410-80e5-81984dd5ec66>
2003-11-15 09:03:44 +0000

    Final touches for release 0.2.1.

--- driconf/trunk/setup.py
+++ driconf/trunk/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="driconf",
-      version="0.2.0",
+      version="0.2.1",
       description="DRI Configuration GUI (gtk-2.0)",
       author="Felix Kuehling",
       author_email="fxkuehl@gmx.de",
