e3977f62ba32b46baed8067088f6b3190c6a476e
nobody <nobody@b79d2f87-9f1e-0410-80e5-81984dd5ec66>
2005-03-18 11:57:47 +0000

    This commit was manufactured by cvs2svn to create tag 'REL-0-2-4'.

---
+++ driconf/tags/REL-0-2-4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name="driconf",
+      version="0.2.4",
+      description="DRI Configuration GUI (gtk-2.0)",
+      author="Felix Kuehling",
+      author_email="fxkuehl@gmx.de",
+      url="http://dri.sourceforge.net/cgi-bin/moin.cgi/DriConf",
+      py_modules=["dri", "driconf", "driconf_xpm"],
+      scripts=["driconf"])
+
+errors = 0
+try:
+    import pygtk
+    pygtk.require ("2.0")
+    import gtk
+except:
+    print "Warning: importing gtk version 2.0 doesn't work."
+    errors = 1
+try:
+    import xml.parsers.expat
+except:
+    print "Warning: importing xml.parsers.expat doesn't work."
+    errors = 1
+
+if errors:
+    print "Warning: driconf will probably not work for the above reason(s)."
